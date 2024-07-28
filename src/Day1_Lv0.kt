32d776758d45cdbfc00efa9269821716001cd5c4
stdiodh <playlistdh@gmail.com>
2024-07-28 11:38:25 +0900

    Day 1

--- src/Day1_Lv0.kt
+++ src/Day1_Lv0.kt
@@ -1,3 +1,15 @@
-fun main(){
-    print("Hello world!")
+//fun main(){
+//    var year = readlnOrNull()!!.toInt()
+//    year -= 543
+//    print("$year")
+//}
+//
+//fun main() {
+//    val name = readlnOrNull()
+//    print("$name??!")
+//}
+
+fun main() {
+    val (a, b, c) = readln().split(" ").map { it.toLong() }
+    print(a + b + c)
 }
