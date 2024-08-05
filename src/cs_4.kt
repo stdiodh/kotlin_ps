e39811bb39ef70d5099bf3c46875767ad4a588be
stdiodh <playlistdh@gmail.com>
2024-08-05 13:10:20 +0900

    ps_4

---
+++ src/cs_4.kt
@@ -0,0 +1,65 @@
+//import java.util.Scanner
+//
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    val N = sc.nextInt()
+//    val M = sc.nextInt()
+//    val arrayN = IntArray(N)
+//    val result = StringBuilder()
+//
+//    for(m: Int in 0 until M) {
+//        val i = sc.nextInt()
+//        val j = sc.nextInt()
+//        val k = sc.nextInt()
+//
+//        for (insert: Int in i..j) {
+//            arrayN[insert - 1] = k
+//        }
+//    }
+//
+//    for (i in arrayN) {
+//        result.append("$i ")
+//    }
+//
+//    println(result)
+//    sc.close()
+//}
+
+//import java.util.Scanner
+//
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    val N = sc.nextInt()
+//    val M = sc.nextInt()
+//    val arrayN = IntArray(N) {it + 1}
+//    val result = StringBuilder()
+//
+//    for (y : Int in 0 until M){
+//        val i = sc.nextInt()
+//        val j = sc.nextInt()
+//        val k = arrayN[i - 1]
+//        arrayN[i - 1] = arrayN[j - 1]
+//        arrayN[j - 1] = k
+//    }
+//
+//    for (a in arrayN){
+//        result.append("$a ")
+//    }
+//    println(result)
+//    sc.close()
+//}
+
+//import java.util.Scanner
+//
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    val arrayX = IntArray(30) { it + 1 }.toMutableList()
+//
+//    for (x in 1 .. 28) {
+//        val num = sc.nextInt()
+//        if (num in arrayX) {
+//            arrayX.remove(num)
+//        }
+//    }
+//    println("${arrayX[0]}\n${arrayX[1]}")
+//}
