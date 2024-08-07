0ad04ff3971f5ddae9a9f31d7c3a2008b3cc220a
stdiodh <playlistdh@gmail.com>
2024-08-07 16:17:12 +0900

    cs_6

---
+++ src/cs_6.kt
@@ -0,0 +1,40 @@
+import java.util.Scanner
+
+//fun main() {
+//    val str = readlnOrNull()
+//    str?.let { println(it.length) }
+//}
+
+//import java.util.Scanner
+//
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    val case = sc.nextInt()
+//    for (x in 0 until case){
+//        val str = sc.next()
+//        println("${str[0]}${str[str.lastIndex]}")
+//    }
+//}
+
+//fun main() {
+//    println("${readln().first().code}")
+//}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val num = sc.next()
+    var total = 0
+
+    for (x in 0 until  n){
+        total += num[x] - '0'
+    }
+
+//    다른 풀이
+//    for (x in num){
+//        total += (x - '0')
+//    }
+//    println(total)
+
+    println(total)
+}
