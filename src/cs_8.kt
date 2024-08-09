708db9d6dbd9e0791aa2a15fb37d1044eaa4b838
stdiodh <playlistdh@gmail.com>
2024-08-09 13:01:18 +0900

    cs_8

---
+++ src/cs_8.kt
@@ -0,0 +1,38 @@
+//import java.util.Scanner
+//
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    val n = sc.next().reversed()
+//    val m = sc.next().reversed()
+//
+//    if (n > m){ println(n) }
+//    else { println(m) }
+//}
+//
+//fun main() {
+//    val str = readlnOrNull()
+//    var time = 0
+//
+//    for (x in str!!.indices)
+//    {
+//        when (str[x])
+//        {
+//            'A', 'B', 'C' -> time += 3
+//            'D', 'E', 'F' -> time += 4
+//            'G', 'H', 'I' -> time += 5
+//            'J', 'K', 'L' -> time += 6
+//            'M', 'N', 'O' -> time += 7
+//            'P', 'Q', 'R', 'S' -> time += 8
+//            'T', 'U', 'V' -> time += 9
+//            'W', 'X', 'Y', 'Z' -> time += 10
+//        }
+//    }
+//    print(time)
+//}
+//
+//fun main(){
+//    while (true) {
+//        val input = readlnOrNull() ?: break
+//        println(input)
+//    }
+//}
