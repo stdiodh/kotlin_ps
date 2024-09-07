fa66470f8ab27c67fe9eb7deb6caca2d2706ff82
stdiodh <playlistdh@gmail.com>
2024-09-07 14:51:39 +0900

    Code Camp Day1

---
+++ src/programmers/Lv0/Day1.kt
@@ -0,0 +1,19 @@
+package programmers.Lv0
+
+class Solution {
+    fun solution(num1: Int, num2: Int) = num1 % num2
+}
+
+class Soluction{
+    fun soluction(num1: Int, num2: Int) = if (num1 == num2) 1 else -1
+}
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        return (num1 - num2)
+    }
+}
+
+class Solution {
+    fun solution(num1: Int, num2: Int) = (num1 * num2)
+}
