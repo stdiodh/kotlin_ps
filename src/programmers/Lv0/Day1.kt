4d9ec080327d423d1c5e856b14c157968db714cc
stdiodh <playlistdh@gmail.com>
2024-09-09 13:12:48 +0900

    Day 1

--- src/programmers/Lv0/Day1.kt
+++ src/programmers/Lv0/Day1.kt
@@ -1,19 +1,27 @@
 package programmers.Lv0
 
-class Solution {
-    fun solution(num1: Int, num2: Int) = num1 % num2
-}
-
-class Soluction{
-    fun soluction(num1: Int, num2: Int) = if (num1 == num2) 1 else -1
-}
-
-class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        return (num1 - num2)
-    }
-}
-
-class Solution {
-    fun solution(num1: Int, num2: Int) = (num1 * num2)
-}
+//class Solution {
+//    fun solution(num1: Int, num2: Int) = num1 % num2
+//}
+//
+//class Solution {
+//    fun solution(age: Int): Int {
+//        var answer : Int = 0
+//        answer = 2022 - age + 1
+//        return answer
+//    }
+//}
+//
+//class Soluction{
+//    fun soluction(num1: Int, num2: Int) = if (num1 == num2) 1 else -1
+//}
+//
+//class Solution {
+//    fun solution(num1: Int, num2: Int): Int {
+//        return (num1 - num2)
+//    }
+//}
+//
+//class Solution {
+//    fun solution(num1: Int, num2: Int) = (num1 * num2)
+//}
