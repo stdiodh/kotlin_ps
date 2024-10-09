9070b4a25241d07d1d9dc079cd9ebe6107d232f0
stdiodh <playlistdh@gmail.com>
2024-10-09 10:07:27 +0900

    ps_12

---
+++ src/ps11_20/ps12/2941.kt
@@ -0,0 +1,13 @@
+package ps11_20.ps12
+
+/* 2941번
+fun main() {
+    val wordList = arrayOf("c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=")
+    var word = readln()
+
+    wordList.forEach {
+        word = word.replace(it, " ")
+    }
+    println(word.length)
+}
+ */
