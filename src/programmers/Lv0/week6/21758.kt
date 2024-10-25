ef2dadb7c57fd63db9ba841ae92c919d2b85db89
stdiodh <playlistdh@gmail.com>
2024-10-25 15:13:36 +0900

    못 푼 문제 풀이 추가

--- src/programmers/Lv0/week6/21758.kt
+++ src/programmers/Lv0/week6/21758.kt
@@ -9,33 +9,43 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
         return nval.toInt()
     }
 
+    //1. 입력을 받는다.
     val n = nextInt()
     val pSum = IntArray(n+1) {0}
     //꿀통의 배열
     val arr = IntArray(n) {
         nextInt()
     }
-
-    //꿀통의 누적합
+    //꿀 배열의 누접합을 구해서 연산을 수행
     for (i in 1 .. n) {
-        pSum[i] = pSum[i - 1] + arr[i - 1]
+        pSum[i] = pSum[i - 1] + arr[i]
     }
 
 
+    //2. 벌이 꿀을 채취할 수 있는 경우의 수를 순회
     var ans = 0
-    //벌통이 중앙에 있는 경우
+    //2-1. 벌통이 오른쪽 끝에 있는 경우
     for (i in 2 ..< n) {
-        ans = max(ans, 2 * pSum[n] - pSum[1] - pSum[i] - arr[i - 1])
+        //arr은 자기 자신의 위치를 채취하지 못함
+        val left = pSum[n] - pSum[1] - arr[i - 1]
+        val right = pSum[n] - pSum[i]
+        val total = left + right
+        ans = max(ans, total)
     }
 
-    //벌통이 가장 왼쪽에 있는 경우
+    //2-2. 벌통이 왼쪽 끝에 있는 경우
     for (i in 2 ..< n) {
-        ans = max(ans, pSum[n - 1] + pSum[i - 1] - arr[i - 1])
+        val right = pSum[n - 1] - arr[i - 1]
+        val left = pSum[i]
+        val total = right + left
+        ans = max(ans, total)
     }
 
-    //벌통이 가장 오른쪽에 있는 경우
+    //2-3. 벌통이 가운데 있을 때 경우에서 최대값.
     for (i in 2 ..< n) {
-        ans = max(ans, pSum[i] - pSum[1] + pSum[n - 1] - pSum[i - 1])
+        //pSum[i - 1]은 벌통의 위치
+        val total = pSum[i] - pSum[1] + pSum[n - 1] - pSum[i - 1]
+        ans = max(ans, total)
     }
 
     println(ans)
--- src/programmers/Lv0/week6/3020.kt
+++ src/programmers/Lv0/week6/3020.kt
@@ -8,10 +8,14 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
         return nval.toInt()
     }
 
+    //장애물판 길이 n 높이가 h
     val n = nextInt(); val h = nextInt()
+    //line 장애물의 누적합
     val line = IntArray(h) {0}
     repeat(n) { idx ->
+        //장애물 사이즈
         val size = nextInt()
+        //2로 나눠지는 것은 종류석 아니면 석순
         if (idx % 2 == 0) {
             line[h - size] += 1
         } else {
@@ -19,10 +23,13 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
             line[size] -= 1
         }
     }
+
+    //장해물의 누적합
     for (i in 1 ..< h) {
         line[i] += line[i - 1]
     }
 
+    //가장 작은 충돌 횟수, 그 구간횟수를 출력
     val ans = line.min(); var cnt = 0
     line.forEach { l ->
         if (l == ans) { cnt += 1 }
