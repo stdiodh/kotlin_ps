325d58b97a5b3bb320b6749d5c67d1731fefdab1
stdiodh <playlistdh@gmail.com>
2024-10-31 18:38:24 +0900

    13900

---
+++ src/programmers/Lv0/week8/13900.kt
@@ -0,0 +1,30 @@
+package programmers.Lv0.week8
+
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int{
+        nextToken()
+        return nval.toInt()
+    }
+
+    val n = nextInt()
+
+    val arr = IntArray(n){
+        nextInt()
+    }
+
+    val pSum = IntArray(n+1){
+        0
+    }
+
+    repeat(n){
+        pSum[it+1] = pSum[it] + arr[it]
+    }
+
+    var result = 0
+    for(x in 0 until n){
+        result += arr[x] * (pSum[n] - pSum[x+1]);
+    }
+    println(result)
+}
