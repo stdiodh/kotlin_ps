b77bdb861d57a80c6bfb3b84e58954ecff62dbdd
stdiodh <playlistdh@gmail.com>
2024-11-01 14:29:19 +0900

    16507 속도개선

--- src/programmers/Lv0/week6/16507.kt
+++ src/programmers/Lv0/week6/16507.kt
@@ -12,9 +12,9 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
 
     val pSum = Array(r+1) { IntArray (c+1) { 0 } }
 
-    repeat(r){idx ->
-        repeat(c) {jdx ->
-            pSum[idx+1][jdx+1] = pSum[idx + 1][jdx] + pSum[idx][jdx + 1] - pSum[idx][jdx] + nextInt()
+    repeat(r){i ->
+        repeat(c){j->
+         pSum[i+1][j+1] = pSum[i+1][j] + pSum[i][j+1] - pSum[i][j] + nextInt()
         }
     }
 
@@ -24,11 +24,8 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
         val c1 = nextInt()
         val r2 = nextInt()
         val c2 = nextInt()
-
-        var count = 0
         //직사각형 넓이 구하는 공식
         val sq = (r2-r1+1) * (c2-c1+1)
-
         sb.appendLine((pSum[r2][c2] - pSum[r2][c1-1] - pSum[r1-1][c2] + pSum[r1-1][c1-1]) / sq)
     }
     println(sb)
