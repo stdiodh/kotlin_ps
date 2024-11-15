b3bead9ab220738651f27b16b1915b552528cf9f
stdiodh <playlistdh@gmail.com>
2024-11-15 15:34:30 +0900

    1012

--- src/programmers/Lv0/week11/1012.kt
+++ src/programmers/Lv0/week11/1012.kt
@@ -8,9 +8,12 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
     //1. 입력을 받는다.
     //가로 M, 세로 N, 배추가 심어져 있는 위치의 개수 k
     repeat(nextInt()){
+        val dx = intArrayOf(1,0,-1,0)
+        val dy = intArrayOf(0,1,0,-1)
+
         val m = nextInt(); val n = nextInt(); val k = nextInt()
-        val graph = Array(m) { IntArray(n) { 0 } }
-        val visited = BooleanArray(graph.size){false}
+        val graph = Array(n) { IntArray(m) { 0 } }
+        val visited = Array(n) { BooleanArray(m) {false} }
 
         repeat(k){
             val x = nextInt(); val y = nextInt()
@@ -18,20 +21,30 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
         }
 
         //2차원 배열을 깊이탐색
-        var ans = 0
         fun dfs(x:Int, y:Int) : Unit{
-            for (n in graph[x]){
-                for(m in graph[y]){
-                    if(!visited[n]){
-                        ans++
-                        visited[n] = true
-                        dfs(n,m)
-                    }
+            graph[x][y] = 0
+            visited[x][y] = true
+            for (i in 0..<4){
+                val nx = x + dx[i]
+                val ny = y + dy[i]
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m){
+                    continue
+                }
+                if(!visited[nx][ny] && graph[nx][ny] == 1){
+                    dfs(nx,ny)
                 }
             }
         }
-        visited[0] = true
-        dfs(0,0)
+
+        var ans = 0
+        for(x in 0..<n){
+            for(y in 0 ..<m){
+                if(graph[x][y] == 1 && !visited[x][y]){
+                    ans++
+                    dfs(x,y)
+                }
+            }
+        }
         println(ans)
     }
 }
