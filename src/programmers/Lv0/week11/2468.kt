7d17518d7ffb6ae8350534e93e6de80e13043804
stdiodh <playlistdh@gmail.com>
2024-11-20 12:44:17 +0900

    2468, 2583 문제 해설 추가

--- src/programmers/Lv0/week11/2468.kt
+++ src/programmers/Lv0/week11/2468.kt
@@ -44,3 +44,58 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
 
     println(maxSafeArea)
 }
+
+
+/*
+11월 18일 Daily 문제 정답 공개
+문제에서 요구하는 안전 영역은 특정 강수량 이상의 높이를 가진 영역으로 구성된 부분입니다.
+이는 DFS 알고리즘을 통해  2차원배열을 탐색하면서 강수량 이상의 높이로 구성된 영역을 식별하면 됩니다.
+설명이 어려울 수 있으니 조금 풀자면, 강수량이 1인 경우 1보다 높은 모든 영역은 안전 영역입니다.
+이러한 안전영역의 덩어리를 구해야하기 때문에 DFS를 수행하여 2차원 배열 순회중 안전 영역인 땅을 만나면 해당 영역과 좌우 상하로 붙어있는
+모든 영역을 방문처리하고 카운팅합니다.
+이러한 과정을 강수량의 범위에서 수행하면 됩니다. 영역의 높이는 최소 1부터 최대 100이므로 강수량이 1부터 100까지 모든 경우를 확인해보면 됩니다.
+해당 경우를 확인하며 정답 변수에 최대값을 전달하면 정답을 출력할 수 있습니다.
+안전영역의 개수가 최대 0인 경우가 분명히 있을 수 있습니다. 따라서, 0인 경우에는 반드시 1개의 안전영역을 갖는 것이 최대라고 출력해줘야 합니다.
+
+import java.io.StreamTokenizer
+import kotlin.math.max
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    fun nextInt() : Int {
+        nextToken()
+        return nval.toInt()
+    }
+    val n = nextInt()
+    val graph = Array(n) {
+        IntArray(n) { nextInt() }
+    }
+    val dx = arrayOf(0, 1, 0, -1); val dy = arrayOf(1, 0, -1, 0)
+    var visited = Array(n) { BooleanArray(n) { false } }
+    fun dfs(x : Int, y : Int, h : Int) {
+        for(i in 0 ..3) {
+            val nx = x + dx[i]; val ny = y + dy[i]
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) { continue }
+            if (!visited[nx][ny] && graph[nx][ny] > h) {
+                visited[nx][ny] = true
+                dfs(nx, ny, h)
+            }
+        }
+    }
+    var ans = 0
+    repeat(100) { h ->
+        var cnt = 0
+        repeat(n) { x ->
+            repeat(n) { y ->
+                if (!visited[x][y] && graph[x][y] > h) {
+                    cnt ++
+                    visited[x][y] = true
+                    dfs(x, y, h)
+                }
+            }
+        }
+        ans = max(ans, cnt)
+        visited = Array(n) { BooleanArray(n) { false } }
+    }
+    print(if (ans > 0) ans else 1)
+}
+ */
--- src/programmers/Lv0/week11/2583.kt
+++ src/programmers/Lv0/week11/2583.kt
@@ -53,4 +53,62 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
     sb.appendLine("${ans.size}")
     ans.forEach { sb.append("$it ") }
     println(sb)
+
+    /*
+    11월 19일 Daily 문제 답안 공개
+    이 문제는 문제에서 주어진 모든 것을 구현하면 해결할 수 있습니다.
+    그 전에 문제에서 요구하는 좌표에 대해서 이해해봅시다. 좌표는 왼쪽 하단부터 오른쪽 상단으로 이어집니다. 이는 좌표 평면이 단순하게 회전된 형태로 다시 회전시키면 일반 좌표로 볼 수 있습니다.
+    그래프를 모두 1 또는 0으로 초기화합니다. 그리고 주어진 좌표 쿼리를 통해 해당 좌표들을 모두 원래의 값과 다른 값을 할당합니다.
+    이제 dfs를 수행합니다. dfs를 수행하면서 초기화된 값이 변하지 않았다면 색칠이 되지 않았기에 정답 배열을 선언하여 너비를 카운팅하고 추가해줍니다.
+    문제에서는 영역의 개수와 각각의 넓이의 오름차순 정렬을 출력합니다. 그렇기에 정답 배열의 크기와 정답 배열을 정렬하여 단순하게 1줄로 모두 출력해주면 됩니다.
+
+    import java.io.StreamTokenizer
+
+    fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+        fun nextInt() : Int { nextToken(); return nval.toInt() }
+        val m = nextInt(); val n = nextInt(); val k = nextInt()
+        var graph = Array(n) { IntArray(m) {0} }
+        var visited = Array(n) { BooleanArray(m) {false} }
+        repeat(k) {
+            val x1 = nextInt(); val y1 = nextInt(); val x2 = nextInt(); val y2 = nextInt()
+            for (x in x1..<x2) {
+                for (y in y1 ..< y2) {
+                    graph[x][y] = 1
+                }
+            }
+        }
+        var result = mutableListOf<Int>()
+        var cnt = 1
+        val dx = arrayOf(0, 1, 0, -1)
+        val dy = arrayOf(1, 0, -1, 0)
+        fun dfs(x : Int, y : Int) : Unit {
+            for (i in 0 .. 3) {
+                val nx = x + dx[i]; val ny = y + dy[i]
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) { continue }
+                if (!visited[nx][ny] && graph[nx][ny] == 0) {
+                    visited[nx][ny] = true
+                    cnt ++
+                    dfs(nx, ny)
+                }
+            }
+        }
+        repeat(n) {x ->
+            repeat(m) {y ->
+                if (!visited[x][y] && graph[x][y] == 0) {
+                    visited[x][y] = true
+                    dfs(x, y)
+                    result.add(cnt)
+                    cnt = 1
+
+                }
+            }
+        }
+        val sb = StringBuilder()
+        sb.appendLine(result.size)
+        result.sorted().forEach { s ->
+            sb.append("$s ")
+        }
+        print(sb)
+    }
+     */
 }
