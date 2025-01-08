0232eb2a1bd1d93e65d6f84900e2f0a2242d2c47
stdiodh <playlistdh@gmail.com>
2025-01-08 10:18:34 +0900

    1010

--- src/programmers/Lv0/week18/1010.kt
+++ src/programmers/Lv0/week18/1010.kt
@@ -38,3 +38,37 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
     }
     print(sb)
 }
+
+/**
+ * n == 1인 경우 m의 값만큼의 경우의 수만 나타납니다.
+ * n > 2인 경우부터는 m개의 지점 중 n개만 선택할 수 있다면, 다리를 놓을 수 있습니다.
+ * 따라서, mCn 과 같습니다. 다만, 해당 연산을 수행하는 경우 지속적으로 !(팩토리얼) 연산을 수행하는 것은 비효율적이므로, 관계식을 통해서
+ * 점화식으로 표현해봅시다. 파스칼의 삼각형을 통해서 아래와 같은 관계식을 알 수 있습니다.
+ * mCn = mC(n - 1) + (m-1)C(n-1)
+ * 이 점화식을 통해서 dp[n][m] = dp[n][m-1] + dp[n-1][m-1] 을 도출할 수 있습니다.
+ *
+ *
+ * import java.io.StreamTokenizer
+ *
+ * fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+ *     fun nextInt() : Int { nextToken(); return nval.toInt() }
+ *     val dp = Array(30) { LongArray(30) {0} }
+ *     for (i in 1 .. 29) {
+ *         dp[1][i] = i.toLong()
+ *     }
+ *     for (i in 2 .. 29) {
+ *         dp[i][i] = 1
+ *     }
+ *     for (i in 2 .. 29) {
+ *         if (i == 29) { continue }
+ *         for (j in i + 1 .. 29) {
+ *             dp[i][j] += dp[i][j - 1] + dp[i - 1][j - 1]
+ *         }
+ *     }
+ *     val sb = StringBuilder()
+ *     repeat(nextInt()) {
+ *         sb.appendLine(dp[nextInt()][nextInt()])
+ *     }
+ *     print(sb)
+ * }
+ */
