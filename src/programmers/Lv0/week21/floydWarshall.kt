88d00122a08a37b2f0ed11cc2e8daf7ea14dc53a
stdiodh <playlistdh@gmail.com>
2025-02-07 15:53:23 +0900

    floydWarshall

---
+++ src/programmers/Lv0/week21/floydWarshall.kt
@@ -0,0 +1,52 @@
+package programmers.Lv0.week21
+
+import kotlin.math.min
+
+fun main() {
+    val graph = listOf(
+        emptyList(),
+        listOf(Pair(2, 4), Pair(4, 6)),
+        listOf(Pair(1, 3), Pair(3, 7)),
+        listOf(Pair(1, 5) , Pair(4, 4)),
+        listOf(Pair(3, 2))
+    )
+    val INF = 1_000_000_000L
+    //최단 경로 기록 2차원 배열선언
+    val dist = Array(5) { LongArray(5) { INF } }
+
+    //자기 자신으로 가는 최단 경로는 0으로 초기화
+    for (i in 1 .. 4){
+        dist[i][i] = 0
+    }
+
+    //그래프 정보를 통해 인접 노드 가중치 초기화
+    for(i in 1 .. 4){
+        for((node, weight) in graph[i]){
+            dist[i][node] = weight.toLong()
+        }
+    }
+
+    //플로이드 워셜
+//    for(k in 1..4){
+//        for(i in 1..4){
+//            for(j in 1..4){
+//                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+//            }
+//        }
+//    }
+
+    repeat(4){k ->
+        repeat(4){i ->
+            repeat(4){j ->
+                dist[i + 1][j + 1] = min(dist[i + 1][j + 1], dist[i + 1][k + 1] + dist[k + 1][j + 1])
+            }
+        }
+    }
+
+    for(i in 1..4){
+        for(j in 1..4){
+            print("${dist[i][j]} ")
+        }
+        println()
+    }
+}
